dd51588e66e811ba6fd2859ba62a1e7343ba8560
TheMushyPeas <callum.bloomfield01@gmail.com>
2014-02-28 20:28:29 +0000

    Implemented /tpto
    Have added a "shortcut" command that allows for quicker teleporting. Usage: /tpto <playername> Requires permission "mbes.cmds.tpto"

--- src/plugins/mbes/Commands/Tpto.java
+++ src/plugins/mbes/Commands/Tpto.java
@@ -1,1 +1,61 @@
-// TODO: ADD THE /TPTO COMMAND - MUSHY
+package Commands;
+
+import com.mbserver.api.CommandExecutor;
+import com.mbserver.api.CommandSender;
+import com.mbserver.api.game.Location;
+import com.mbserver.api.game.Player;
+import com.mbserver.api.Server;
+
+
+public class Tpto implements CommandExecutor {
+	private Server server;
+
+    public Tpto( Server server ) {
+        this.server = server;
+    }
+    @Override
+    public void execute( String command, CommandSender sender, String[] args, String label ) {
+        Player player = (Player)sender;
+        Player sendplayer = server.getPlayerExact(args[0]);
+        
+        if (sender.hasPermission("mbes.cmds.tpto")){
+        	
+        		
+        			
+        			if (sendplayer != null){
+        				
+        			    Location loc = sendplayer.getLocation();
+                		
+                		player.teleport(loc);
+                		
+                		sender.sendMessage("You were teleported to " + args[0]);
+                		
+                	}
+                	
+                	else{
+                		
+                		
+                		
+        				sender.sendMessage("The specified player could not be found");
+                		
+                		
+                		
+                		
+        			
+        	}
+        	
+        	
+        				
+        	
+        	
+        }
+        
+        
+        
+        else {
+        	sender.sendMessage("You don't have permission to use this command!");
+        }
+        
+    }
+
+}
