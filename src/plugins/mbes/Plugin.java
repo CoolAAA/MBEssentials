3d8a686128fcc112a043fe770b4da48b400aca16
TheMushyPeas <callum.bloomfield01@gmail.com>
2014-02-28 20:55:12 +0000

    Updated main plugin class
    I updated the main plugin class to reflect my recent command addition, "/tpto"

--- src/plugins/mbes/Plugin.java
+++ src/plugins/mbes/Plugin.java
@@ -2,18 +2,29 @@ package plugins.mbes;
 
 import com.mbserver.api.MBServerPlugin;
 import com.mbserver.api.Manifest;
+import plugins.mbes.commands.Tpto;
+
+
 
 @Manifest(name="MBEssentials",authors = {"TheMushyPeas","AAAA"})
 public class Plugin extends MBServerPlugin{
 	@Override
 	public void onEnable() {
-		this.getlogger().info("Thanks for using MBEssentials by AAAA and TheMushypeas!");
-		this.getlogger().info("Please report any bugs and glitches to the forums!");
+		this.getLogger().info("Thanks for using MBEssentials by AAAA and TheMushypeas!");
+		this.getLogger().info("Please report any bugs and glitches to the forums!");
         this.getPluginManager().registerCommand("kill",new Commands(this.getServer()));
-		this.getlogger().info("Successfully registered command: /kill");
+		this.getLogger().info("Successfully registered command: /kill");
 		this.getPluginManager().registerCommand("sudo",new ModCmds(this.getServer()));
-		this.getlogger().info("Successfully registered command: /sudo");
+		this.getLogger().info("Successfully registered command: /sudo");
 		this.getPluginManager().registerCommand("kickall",new ModCmds(this.getServer()));
-		this.getlogger().info("Successfully registered command: /kickall");
+		this.getLogger().info("Successfully registered command: /kickall");
+		this.getPluginManager().registerCommand("tpto",new Tpto(this.getServer()));
+		this.getLogger().info("Successfully registered command: /tpto");
+		
+		this.getLogger().info("MBEssentials Startup Finished!");
+	}
+	@Override
+	public void onDisable(){
+		this.getLogger().info("Have a nice day - from the MBEssentials Team!");
 	}
 }
