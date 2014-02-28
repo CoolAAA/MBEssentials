2c7f68534f4977761e3adca15556b333b41c7c3c
TheMushyPeas <callum.bloomfield01@gmail.com>
2014-02-28 21:09:43 +0000

    Cleaned up the formatting

--- src/plugins/mbes/commands/Tpto.java
+++ src/plugins/mbes/commands/Tpto.java
@@ -6,7 +6,6 @@ import com.mbserver.api.game.Location;
 import com.mbserver.api.game.Player;
 import com.mbserver.api.Server;
 
-
 public class Tpto implements CommandExecutor {
 	private Server server;
 
@@ -20,38 +19,24 @@ public class Tpto implements CommandExecutor {
         
         if (sender.hasPermission("mbes.cmds.tpto")){
         	
-        		
-        			
-        			if (sendplayer != null){
+        	if (sendplayer != null){
         				
-        			    Location loc = sendplayer.getLocation();
+        		Location loc = sendplayer.getLocation();
                 		
-                		player.teleport(loc);
+                	player.teleport(loc);
                 		
-                		sender.sendMessage("You were teleported to " + args[0]);
+                	sender.sendMessage("You were teleported to " + args[0]);
                 		
                 	}
                 	
-                	else{
-                		
-                		
-                		
-        				sender.sendMessage("The specified player could not be found");
-                		
-                		
-                		
-                		
-        			
+                else{
+                	
+        		sender.sendMessage("The specified player could not be found");
+
         	}
-        	
-        	
-        				
-        	
-        	
+
         }
-        
-        
-        
+
         else {
         	sender.sendMessage("You don't have permission to use this command!");
         }
