6e1731ef8e5ba41126c353f856ff875620772412
Abiram <abiram26@hotmail.com>
2014-03-01 15:25:52 +0100

    Update Config.java
    Is this ok? :D

--- src/plugins/mbes/Config.java
+++ src/plugins/mbes/Config.java
@@ -3,3 +3,21 @@
 //PLANNED CONFIGURATIONS: (PUT ALL PLANNED CONFIGS HERE)
 // *enable-money enables / disables the money system
 // *
+
+package a.package.name; //idk the name o.O
+
+public class Config {
+	 	private boolean enableMoneySystem;
+	 	
+	 	 	public Config() {
+	        this.setEnableMoneySystem(false);
+	 	}
+	 		 	public boolean isEnableMoneySystem() {
+			return this.enableMoneySystem;
+		}
+		public void setEnableMoneySystem(boolean in) {
+			this.enableMoneySystem=in;
+		}
+	 	
+}
+	 	
