4a9ded83ac319ccf4cf6ee5fdf5008da79414fc9
TheMushyPeas <callum.bloomfield01@gmail.com>
2014-04-10 08:27:04 +0100

    Changed dependencies
    Again, this would have broken. Also changed the MBEPlugin variable name to "mbes" to make it slightly more understandable for the begginers.

--- Api Examples/WordReplacerExample.java
+++ Api Examples/WordReplacerExample.java
@@ -7,24 +7,24 @@ import com.mbserver.api.MBServerPlugin;
 import com.mbserver.api.Manifest;
 
 
-@Manifest(name = "Test",dependencies = "MBEssentials")
+@Manifest(name = "Test",dependencies = "MbEssentials")
 public class WordReplacerExample extends MBServerPlugin{
 	
 	@Override
 	public void onEnable() {
 		//Get the plugin from the plugins. Use the static field in MBEPlugin.
-		MBEPlugin plugin = (MBEPlugin)this.getPluginManager().getPlugin(MBEPlugin.MANIFEST_NAME);
+		MBEPlugin mbes = (MBEPlugin)this.getPluginManager().getPlugin(MBEPlugin.MANIFEST_NAME);
 		
 		//Get the chat replacer
 		ChatReplacer chat = plugin.getChatReplacer();
 		
 		
 		//Add the word to the chat replacer 
-		chat.addWord("MB", "Minebuilder");
+		mbes.addWord("MB", "Minebuilder");
 		
 		//If someone in the chat says "MB" it will change it to Minebuilder.
 		
-		chat.delWord("MB");
+		mbes.delWord("MB");
 		
 		//Now if someone says "MB" it will not change it into minebuilder.
 	}
