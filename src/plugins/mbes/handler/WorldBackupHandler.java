cd52ef15ce51fbcb7c71dab95b591f77016e33cf
Abiram <abiram26@hotmail.com>
2014-04-13 15:41:11 +0200

    !!URGENT!!
    ~~!!URGENT!!!~~
    Look at comments

--- src/plugins/mbes/handler/WorldBackupHandler.java
+++ src/plugins/mbes/handler/WorldBackupHandler.java
@@ -14,6 +14,15 @@ public class WorldBackupHandler implements Listener{
 	@EventHandler(concurrency = RunMode.THREADED)
 	public void onWorldSave(WorldSaveEvent e){
 		try {
+			
+			//So every 5 minutes (=WorldSaveEvent) there will be a backup of the World...
+			//I do not think this is a very good idea
+			//1. This will create noticable lagg.
+			//2a. World folders are actually not so small, so it will very quickly fill de HDD/SSD.
+			//2b. AeroShark's worldfolder = 500MB (imagine that being copied every 5 minutes o.O)
+			//2c. 500MB*12*24= 144000MB per day = more or less 144,0 GB a day for AeroShark!!!
+			//2d. And to be honest... AeroShark can only hold 100GB xD (That's why I backup it once a day (with a script)
+			//3. I have no idea how long copying would take... but I guess it will hake long.
 			Thread.sleep(20000);
 		} catch (InterruptedException e1) {
 			// XXX Auto-generated catch block
