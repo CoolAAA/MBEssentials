15bd12bbe3793aca7e30f0cb0c302da54f1fa203
TheMushyPeas <callum.bloomfield01@gmail.com>
2014-04-16 08:58:57 +0100

    Added /delnick

---
+++ src/plugins/mbes/commands/DelNickname.java
@@ -0,0 +1,29 @@
+package plugins.mbes.commands;
+
+import plugins.mbes.Config;
+
+import com.mbserver.api.CommandExecutor;
+import com.mbserver.api.CommandSender;
+import com.mbserver.api.game.Player;
+
+public class DelNickname implements CommandExecutor {
+	private Config config;
+
+    public DelNickname(Config config) {
+        this.config = config;
+    }
+	@Override
+	public void execute(String command, CommandSender sender, String[] args,
+			String label) {
+		
+		Player player = null;
+    	player = (Player)sender; 
+    	this.config.setPlayerNickname(player, null);
+    	sender.sendMessage("Your nickname has been cleared!");
+	}
+}
+		    
+
+
+
+	
