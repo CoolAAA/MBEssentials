2c3bea40e26bdc62c1a3a5a76219d2799577b93a
TheMushyPeas <callum.bloomfield01@gmail.com>
2014-04-16 08:58:28 +0100

    Added /nick

---
+++ src/plugins/mbes/commands/Nickname.java
@@ -0,0 +1,55 @@
+package plugins.mbes.commands;
+
+import plugins.mbes.Config;
+
+import com.mbserver.api.CommandExecutor;
+import com.mbserver.api.CommandSender;
+import com.mbserver.api.game.Player;
+
+public class Nickname implements CommandExecutor {
+	private Config config;
+
+    public Nickname(Config config) {
+        this.config = config;
+    }
+	@Override
+	public void execute(String command, CommandSender sender, String[] args,
+			String label) {
+		
+		Player player = null;
+    	player = (Player)sender; 
+    	
+    	if(sender.hasPermission("mbes.nickname") || sender.hasPermission("mbes.*")){
+    	
+		if (args.length == 0){
+			
+			String nicktemp = this.config.getPlayerNickname(player);
+			if (nicktemp == null){
+				sender.sendMessage("You do not currently have a nickname");
+			}
+			else {
+			sender.sendMessage("Your current nickname is: " + nicktemp );
+			}
+			
+		}
+		 
+		else{
+			
+			this.config.setPlayerNickname(player, args[0]);
+			sender.sendMessage("Your new nickname is: " + this.config.getPlayerNickname(player));
+					
+			
+			}
+    	}
+    	else{
+    		
+    		sender.sendMessage("You do not have permission to use /nick");
+    		
+    	}
+}
+}
+		    
+
+
+
+	
